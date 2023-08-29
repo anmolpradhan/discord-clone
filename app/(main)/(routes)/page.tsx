3e2c103a368028ed6180e14ed5f9aa529acc3ba1
anmolpradhan <anamolpradhan@gmail.com>
2023-08-29 16:02:40 +0545

    Setting up the project

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle/>
+    </div>
+  )
+}
